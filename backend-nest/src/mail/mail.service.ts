62984021e684e075a791da8695eaf61c818e6c7d
pedro4O4 <bebonageh68@gmail.com>
2025-12-29 22:24:57 +0100

    feat: add mail service for sending verification and password reset OTPs via Nodemailer

--- backend-nest/src/mail/mail.service.ts
+++ backend-nest/src/mail/mail.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as nodemailer from 'nodemailer';
+@Injectable()
 export class MailService {
   private transporter: any;
   private fromEmail: string;
